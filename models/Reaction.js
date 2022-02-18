ea4ae1fb110e3f38617d9e47f74a202a4e2f9037
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-18 16:34:46 -0500

    Add Reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,37 @@
-//schema only
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `timestamp` that formats the time
+reactionSchema.virtual('timestamp').get(function () {
+  return new Date(this.createdAt).toDateString();
+});
+
+module.exports = reactionSchema;
